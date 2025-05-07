42d5a2244fc2720b33088a74db39a9a6b21bf56c
Ahmadaltmouri <maltumori@gmail.com>
2025-05-07 13:25:06 +0300

    first commit

---
+++ Managers/BookManager.cs
@@ -0,0 +1,64 @@
+﻿using Pro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pro.Managers
+{
+    public class BookManager
+    {
+        private List<Book> books = new List<Book>();
+
+     
+        public void AddBook(string title, string author)
+        {
+            if (books.Exists(b => b.Title == title))
+            {
+                throw new ArgumentException("The book already exists");
+            }
+            books.Add(new Book { Title = title, Author = author });
+        }
+
+      
+        public bool BorrowBook(User user, string title)
+        {
+            var book = books.Find(b => b.Title == title && b.IsAvailable);
+            if (book == null) return false; 
+
+     
+            book.IsAvailable = false;
+            user.BorrowedBooks.Add(book); 
+            return true;
+        }
+
+
+        public bool ReturnBook(User user, string title)
+        {
+            var book = user.BorrowedBooks.Find(b => b.Title == title);
+            if (book == null) return false; 
+
+     
+            book.IsAvailable = true;
+            user.BorrowedBooks.Remove(book); 
+            return true;
+        }
+
+       
+        public void RemoveBook(string title)
+        {
+            var book = books.Find(b => b.Title == title);
+            if (book == null)
+            {
+                throw new ArgumentException("Book not found.");
+            }
+            books.Remove(book);
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return books;
+        }
+    }
+}
---
+++ Managers/UserManager.cs
@@ -0,0 +1,57 @@
+﻿using Pro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pro.Managers
+{
+    public class UserManager
+    {
+        private List<User> users = new List<User>();
+          
+        public UserManager()
+        {
+            users=new List<User>() { 
+             new User { Username = "admin", Password = "admin" },
+             new User { Username = "user", Password = "user" },
+             new User { Username = "guest", Password = "guest" }
+            
+            };
+        }
+
+     
+        public void RegisterUser(string username, string password)
+        {
+            if (users.Exists(u => u.Username == username))
+            {
+                throw new ArgumentException("Username already exists.");
+            }
+            users.Add(new User { Username = username, Password = password });
+        }
+
+       
+        public User LoginUser(string username, string password)
+        {
+            var user = users.Find(u => u.Username == username && u.Password == password);
+            return user;
+        }
+
+     
+        public void RemoveUser(string username)
+        {
+            var user = users.Find(u => u.Username == username);
+            if (user == null)
+            {
+                throw new ArgumentException("User not found.");
+            }
+            users.Remove(user);
+        }
+
+        public User SearchUser(string username)
+        {
+            return users.Find(u => u.Username == username);
+        }
+    }
+}
